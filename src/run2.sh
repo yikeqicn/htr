c8a7a4255e236952bb60d3a151d4d7c8de5b0f58
Ronny <wrhuang@mit.edu>
2018-11-11 23:38:35 -0500

    doing expeirments

--- src/run2.sh
+++ src/run2.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-nohup python main.py --gpu=0 --train --name=densenet-noncustom-beamsearch --densenet --beamsearch --reduction=.4 &
-nohup python main.py --gpu=0 --train --name=densenet-noncustom-beamsearch --densenet --beamsearch --depth=56 &
-nohup python main.py --gpu=1 --train --name=densenet-noncustom-beamsearch --densenet --beamsearch --growth_rate=9 &
-nohup python main.py --gpu=1 --train --name=densenet-noncustom-beamsearch --densenet --beamsearch --growth_rate=7 &
+nohup python main.py --gpu=0 --train --name=reduction --densenet --beamsearch --reduction=.4 &
+nohup python main.py --gpu=0 --train --name=depth --densenet --beamsearch --depth=56 &
+nohup python main.py --gpu=1 --train --name=growthrate-9 --densenet --beamsearch --growth_rate=9 &
+nohup python main.py --gpu=1 --train --name=growthrate-7 --densenet --beamsearch --growth_rate=7 &
+
+nohup python main.py --gpu=2 --train --name=dropout --densenet --beamsearch --keep_prob=.5 &
